3c992624f7443753145aaa8012cb5d7a2f99e6c3
BachoG <bachogmz@gmail.com>
2026-09-24 11:17:25 +0400

    Auth correct apicoockie to apiheader

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,9 @@
 from fastapi import HTTPException, Security
-from fastapi.security import APIKeyCookie
+from fastapi.security import APIKeyHeader
 
 from app.config import API_KEY
 
-api_key_header = APIKeyCookie(name="X-API-Key", auto_error=False)
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 def verify_api_key(api_key: str = Security(api_key_header)):
     if api_key != API_KEY:
